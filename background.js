70036de0428de14cf6af3aa178b1b2ed074b82f2
m-miyano-biglobe <m-miyano@za.biglobe.co.jp>
2018-08-17 17:35:19 +0900

    popup追加

--- background.js
+++ background.js
@@ -1,44 +1,23 @@
 var set_time = 900; // 15min = 900sec
+var count = 0;
+var timer_id = null;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+function countdownStartFunction(tab) {
     alert("Let you know 15 min later!")
-    var count = set_time;
+    count = set_time;
     var countdown = function(){
         count--;
     }
-    var id = setInterval(function(){
+    timer_id = setInterval(function(){
         countdown();
         if(count === 0){
             alert("Time's up!")
-            clearInterval(id);
+            clearInterval(timer_id);
         }}, 1000)
-});
+};
 
-//document.browserAction.addEventListener('DOMContentLoaded', function() {
-//    var link = document.getElementById('startTimer');
-//    link.addEventListener('click', function() {
-//        var count = set_time;
-//        var countdown = function(){
-//            count--;
-//        }
-//        var id = setInterval(function(){
-//            countdown();
-//            if(count === 0){
-//                alert("Times up!")
-//                clearInterval(id);
-//            }}, 1000)
-//    });
-//});
-
-
-//document.getElementById("startTimer").onclick = function(){
-    //var count = set_time;
-    //var countdown = function(){
-    //    console.log(count--);
-    //}
-    //var id = setInterval(function(){
-    //    countdown();
-    //    if(count === 0){
-    //        clearInterval(id);
-    //    }}, 1000)
-//};
+function countdownStopFunction(tab) {
+    clearInterval(timer_id);
+    count = 0;
+    alert("Stopped.")
+};
--- popup.js
+++ popup.js
@@ -1,29 +1,9 @@
-var set_time = 0; // 15min = 900sec
+var bgWindow = chrome.extension.getBackgroundPage();
+
 document.getElementById("startTimer").onclick = function() {
-    var count = set_time;
-    var countdown = function(){
-        //console.log(count--);
-        setInterval(your_function, 1000)
-        var id = setTimeout(countdown, 1000);
-        if(count === 0){
-            clearTimeout(id);
-            alert("Time up!");
-        }
-    }
-
-
+    bgWindow.countdownStartFunction();
 };
 
-//alert("HEY!");
-//chrome.storage.sync.get('time', function(data) {
-//    startTimer.style.backgroundColor = data.time;
-//    startTimer.setAttribute('value', data.time);
-//    startTimer.onclick = function(element) {
-//        let time = element.target.value;
-//        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//            chrome.tabs.executeScript(
-//                tabs[0].id,
-//                {code: 'document.body.style.backgroundColor = "' + time + '";'});
-//       });
-// };
-//});
+document.getElementById("stopTimer").onclick = function() {
+    bgWindow.countdownStopFunction();
+};
