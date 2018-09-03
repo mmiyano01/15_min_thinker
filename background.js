11d3bc8a7ed41432b618b04faacf7118dfe89e26
m-miyano-biglobe <m-miyano@za.biglobe.co.jp>
2018-09-03 17:13:51 +0900

    added jquery and add function to display count down

--- background.js
+++ background.js
@@ -1,9 +1,29 @@
 var set_time = 900; // 15min = 900sec
 var count = 0;
 var timer_id = null;
+var is_running = false;
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (is_running) {
+            switch (request.cmd) {
+                case "is_running":
+                    sendResponse(is_running);
+                    return true;
+                case "count":
+                    sendResponse(count);
+                    return true;
+                default:
+                    break;
+            }
+        }
+        sendResponse("");
+        return true;
+    }
+);
 
 function countdownStartFunction(tab) {
-    alert("Let you know 15 min later!")
+    is_running = true;
     count = set_time;
     var countdown = function(){
         count--;
@@ -11,13 +31,16 @@ function countdownStartFunction(tab) {
     timer_id = setInterval(function(){
         countdown();
         if(count === 0){
+            is_running = false;
             alert("Time's up!")
             clearInterval(timer_id);
         }}, 1000)
+    return true;
 };
 
 function countdownStopFunction(tab) {
+    is_running = false;
     clearInterval(timer_id);
     count = 0;
-    alert("Stopped.")
+    return true;
 };
--- popup.js
+++ popup.js
@@ -1,9 +1,70 @@
 var bgWindow = chrome.extension.getBackgroundPage();
+var timer_id = null;
+
+$(document).ready(function(){
+    chrome.runtime.sendMessage(
+        { cmd: "is_running" },
+        function (is_running) {
+            if (is_running) {
+                setWorkingMode();
+                displayCountDown();
+            }
+            else {
+                setWaitingMode();
+            }
+        }
+    );
+});
 
 document.getElementById("startTimer").onclick = function() {
     bgWindow.countdownStartFunction();
+    setWorkingMode();
+    displayCountDown();
 };
 
 document.getElementById("stopTimer").onclick = function() {
     bgWindow.countdownStopFunction();
+    clearInterval(timer_id);
+    setWaitingMode();
 };
+
+function setWaitingMode(){
+    document.getElementById("waiting").style.display="block";
+    document.getElementById("working").style.display="none";
+    document.getElementById("counter").innerHTML="READY"
+};
+
+function setWorkingMode(){
+    document.getElementById("waiting").style.display="none";
+    document.getElementById("working").style.display="block";
+};
+
+function displayCountDown(num){
+    chrome.runtime.sendMessage(
+        { cmd: "count" },
+        function (count){
+            var countdown = function(){
+                count--;
+                document.getElementById("counter").innerHTML=formatCount(count);
+            }
+            timer_id = setInterval(function(){
+                countdown();
+                if(count === 0){
+                    clearInterval(timer_id);
+                    setWaitingMode();
+                }}, 1000)
+        }
+    );
+};
+
+function formatCount(num) {
+    var result = "";
+    var min = Math.floor((num / 60) % 60);
+    var sec = Math.floor(num % 60);
+
+    if (min > 0) {
+        result += min + ":";
+    }
+    result += ('00' + sec).slice(-2);
+    return result;
+}
